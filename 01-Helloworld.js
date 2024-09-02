bcdee4e906150bf2406789334f7bcb09e5a28ac6
edilson gomes de lima <edskateicevida@gmail.com>
2024-09-02 05:08:13 -0300

    add .

---
+++ 01-Helloworld.js
@@ -0,0 +1,18 @@
+import { GrReturn } from "react-icons/gr";
+
+var num = 1; // {1} declara uma variável em javascript
+num = 3; // {2} atualizamos uma variável existente.
+var price = 1.5; // {3} declaramos igulamente um número, porém, dessa vez, é um número  decimal de pnto flutuante
+var myName = "packt"; // {4} declaramos uma string;
+var trueVale = true; // {5} declaramos um booleano
+var nullVar = null; // {6} declaramos um valor null
+var und; // {7} declaramos uma variável undefined
+console.log("num: " + num);
+console.log("myName:" + myName);
+console.log("trueVale: " + trueVale);
+console.log("price:" + price);
+console.log("nullVar: " + und);
+
+
+
+
